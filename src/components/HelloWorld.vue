b3c1283277836da2ff88a4c4463d71cceaebada4
nansanhao <389746614@qq.com>
2018-08-06 21:36:49 +0800

    做了一个关于拖拽的demo，安装了vue-touter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,66 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <div class="section" v-on:drop="drop($event)" v-on:dragover="allowDrop($event)">
+      <div id="drag-demo" draggable="true" v-on:dragstart="drag($event)" @click="click" >
+
+      </div>
+      
+    </div>
+    <div class="section" v-on:drop="drop($event)" v-on:dragover="allowDrop($event)"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "app",
+  data() {
+    return {
+      title: "first-demo"
+    };
+  },
+  methods: {
+    click(){
+     
+    },
+    drag(ev) {
+      
+      ev.dataTransfer.setData("Text", ev.target.id);
+    },
+    drop(ev) {
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("Text");
+      ev.target.appendChild(document.getElementById(data));
+    },
+    allowDrop(ev) {
+      ev.preventDefault();
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#app {
+  display: flex;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.section {
+  display: flex;
+  width: 50%;
+  height: 400px;
+  background-color: #4caf50;
+  padding: 40px;
+  margin: 20px;
+  border-radius: 16px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.section:hover {
+  box-shadow: 0 0px 20px 0 rgba(0, 0, 0, 0.5), 0 0px 60px 0 rgba(0, 0, 0, 0.19);
+  transition-duration: 0.5s;
 }
-a {
-  color: #42b983;
+#drag-demo {
+  width: 200px;
+  height: 200px;
+  background-color: #35495e;
+  border-radius: 20px;
+  margin: 10px
 }
 </style>
